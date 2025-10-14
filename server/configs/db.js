ee33041debeca4b268546b974512cd3b991d9dfc
osama12589 <muhammadosama1258@gmail.com>
2025-10-14 18:36:44 +0500

    first commit

---
+++ server/configs/db.js
@@ -0,0 +1,11 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected' , ()=> console.log("DB CONNECTED"));
+        await mongoose.connect(`${process.env.MONGODB_URL}/pingup`)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+export default connectDB
