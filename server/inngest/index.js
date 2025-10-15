fc62b05100c8b6993b318a7c24be3fffc214d147
osama12589 <muhammadosama1258@gmail.com>
2025-10-15 21:18:47 +0500

    fix inngest id error

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -23,7 +23,7 @@ const syncUserCreation = inngest.createFunction(
         }
 
         const userData = {
-            __id: id,
+            _id: id,
             email: email_addresses[0].email_address,
             full_name: first_name + " " + last_name,
             profile_picture: image_url,
